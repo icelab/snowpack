a43633e1a86ef4864923f92f363889d560d8c392
Tim Riley <tim@openmonkey.com>
2018-03-27 11:26:51 +1100

    Update Web::Form to work with new Formalist form instance API

--- lib/snowflakes/web/form.rb
+++ lib/snowflakes/web/form.rb
@@ -2,55 +2,34 @@ require "json"
 
 require "formalist"
 require "formalist/elements/standard"
-require "dry/configurable"
 
 module Snowflakes
   module Web
     class Form < ::Formalist::Form
       setting :prefix
 
-      class Result
-        attr_reader :output, :prefix
-
-        def initialize(output, prefix)
-          @output = output
-          @prefix = prefix
-        end
-
-        def to_h(config = {})
-          if config.any?
-            {ast: output.to_ast, prefix: prefix, config: {global: config}}
-          else
-            {ast: output.to_ast, prefix: prefix}
-          end
-        end
-
-        def to_ast
-          output.to_ast
-        end
-
-        def to_s
-          JSON.generate(to_h)
-        end
-      end
-
       def self.prefix(value)
         config.prefix = value
       end
 
-      def build(*args)
-        output =
-          if args.any?
-            super(*args.map(&Kernel.method(:Hash)))
-          else
-            super
-          end
-
-        Result.new(output, prefix)
-      end
-
       def prefix
         self.class.config.prefix
+      end
+
+      def fill(input: {}, errors: {}, **args)
+        super(input: Hash(input), errors: errors, **args)
+      end
+
+      def to_h(config = {})
+        if config.any?
+          {ast: to_ast, prefix: prefix, config: {global: config}}
+        else
+          {ast: to_ast, prefix: prefix}
+        end
+      end
+
+      def to_s
+        JSON.generate(to_h)
       end
     end
   end
