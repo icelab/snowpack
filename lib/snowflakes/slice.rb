fc9c95d89c0d099decac4e8a82d0cdf0dfbe8835
Tim Riley <tim@openmonkey.com>
2019-04-04 20:46:02 +1100

    Rejig code

--- lib/snowflakes/slice.rb
+++ lib/snowflakes/slice.rb
@@ -11,18 +11,17 @@ module Snowflakes
       if klass.superclass == Snowflakes::Slice
         raise "Snowflakes.application not configured yet" unless Snowflakes.application?
 
-        Snowflakes.application.tap do |app|
-          klass.register :inflector, app[:inflector]
-          klass.register :logger, app[:logger]
-          klass.register :rack_monitor, app[:rack_monitor] # do we need this?
+        app = Snowflakes.application
 
-          klass.config.env = app.env
-          klass.config.name = klass.slice_name.to_sym
-          klass.config.root = Pathname(File.join(Dir.pwd, "slices", klass.slice_name))
-          klass.config.default_namespace = klass.slice_namespace_path
-        end
+        klass.register :inflector, app[:inflector]
+        klass.register :logger, app[:logger]
+        klass.register :rack_monitor, app[:rack_monitor] # do we need this?
 
+        klass.config.env = app.env
+        klass.config.name = klass.slice_name.to_sym
+        klass.config.root = Pathname(File.join(Dir.pwd, "slices", klass.slice_name))
         klass.config.auto_register = [File.join("lib", klass.slice_namespace_path)]
+        klass.config.default_namespace = klass.slice_namespace_path
 
         klass.after :configure do
           klass.load_paths! "lib"
