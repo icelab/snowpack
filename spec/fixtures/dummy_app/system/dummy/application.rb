17d2c8477beeb80d274c4fadf17a6b6ff5bdee6a
Tim Riley <tim@openmonkey.com>
2019-04-05 15:00:16 +1100

    Add missing application file to dummy app

---
+++ spec/fixtures/dummy_app/system/dummy/application.rb
@@ -0,0 +1,11 @@
+require "snowflakes/application"
+
+module Dummy
+  class Application < Snowflakes::Application
+    # use :env
+    configure do |config|
+      config.name = :dummy
+      config.root = File.join(__dir__, "../..")
+    end
+  end
+end
